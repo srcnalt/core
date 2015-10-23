a814752735d018c61542fdcf9ac0a71ca4ca89e7
Andrew <andrew@codeusa523.org>
2015-10-23 08:47:19 -0400

    Should fetch the raw mp4 from youtubes servers

--- src/me/aurous/YouTubeTest.java
+++ src/me/aurous/YouTubeTest.java
@@ -1,12 +1,14 @@
 package me.aurous;
 
 import me.aurous.api.auth.impl.youtube.YouTubeDecoder;
+import me.aurous.utils.Internet;
 
 public class YouTubeTest {
 
 
 	public static void main(String args[]) {
-		YouTubeDecoder decoder = new YouTubeDecoder("https://www.youtube.com/watch?v=KEI4qSrkPAs");
+		System.out.println(Internet.text("https://www.youtube.com/results?search_query=look+on+down+from+the+bridge+mazzy+star&filters=video&lclk=video"));
+		YouTubeDecoder decoder = new YouTubeDecoder("https://www.youtube.com/watch?v=p3NZn0mA_XI");
 		System.out.println(decoder.grab());
 	}
 	
